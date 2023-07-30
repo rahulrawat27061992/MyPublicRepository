bdd2155daafcd7cbcd47d3fba52ad38b5ec9e103
RahulRwts <rahulrawat.net@gmail.com>
2023-07-30 18:25:34 +0530

    my first commit

---
+++ AlphaLiaison/AlphaLiaison/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace AlphaLiaison.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
